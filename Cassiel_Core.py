34cba4183af820277aeabddd3c4d2f874fdb63db
Tom Moore <tom@tlear.co.uk>
2017-11-06 21:12:56 +0000

    Cassiel-Dev: Add Server file and rename files to user underscores because Python is a whiny child or something

--- Cassiel_Core.py
+++ Cassiel_Core.py
@@ -1,17 +1,20 @@
 ###############################################################
-# Cassiel-Core.py
+# Cassiel_Core.py
 # (C) Tom Moore 2017
 ###############################################################
 
 ###############################################################
 # Imports
 import sys
+from Cassiel_Server import *
 
 ###############################################################
 # Functions
 
 def main(argv):
-    print "foo"
+	server = Cassiel_Server()
+	server.run()
+    
 
 if __name__ == "__main__":
     main(sys.argv)
